45cc7fe559e64efc641467477dfd1580f1d62054
PRAS <prassamin@gmail.com>
2025-02-20 01:56:36 +0600

    v2.0.1f - Updated Maintenance Ending Time

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -13,7 +13,7 @@ interface MaintenanceNotice {
 export const maintenanceNotice: MaintenanceNotice = {
   enabled: true,
   end: {
-    date: "2025-02-20",
+    date: "2025-02-30",
     probability: "high",
   },
   message: (
