cf44848fd4a59451eab65099332aadfb95c156d5
PRAS <prassamin@gmail.com>
2025-03-03 02:56:12 +0600

    v2.0.1l - added GET support in /chat/invite api

--- src/app/api/chat/invite/route.ts
+++ src/app/api/chat/invite/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { InviteTokenGenerator } from "./handlers";
+import { db } from "@/utils/db";
+import { decodeToken } from "@/utils/tokenizer";
 
 export async function POST(request: NextRequest) {
   const { token } = await request.json();
@@ -9,4 +11,35 @@ export async function POST(request: NextRequest) {
   } else {
     return NextResponse.json({ success, message }, { status });
   }
+}
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get("id");
+  if (!id) {
+    return NextResponse.json(
+      { success: false, message: "ID is required" },
+      { status: 400 }
+    );
+  }
+
+  const inviteDetails: { id: string; token: string } | null =
+    await db.chatInvite.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+  const decodedData = inviteDetails?.token
+    ? decodeToken(inviteDetails.token)
+    : null;
+
+  if (!decodedData) {
+    return NextResponse.json(
+      { success: false, message: "Invite link is expired" },
+      { status: 400 }
+    );
+  }
+
+  return NextResponse.json(decodedData, { status: 200 });
 }
