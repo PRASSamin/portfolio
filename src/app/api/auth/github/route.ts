e863a4611207c96dbd25fdc2451b156b950659b4
PRASSamin <prassamin@gmail.com>
2025-08-07 03:06:05 +0600

    fix: auth redirect url

--- src/app/api/auth/github/route.ts
+++ src/app/api/auth/github/route.ts
@@ -1,7 +1,7 @@
-import { GITHUB_AUTH_CLIENT_ID } from "@/constants/env";
-import { NextResponse } from "next/server";
+import { BASE_URL, GITHUB_AUTH_CLIENT_ID } from "@/constants/env";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  const redirectUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_AUTH_CLIENT_ID}&redirect_uri=http://localhost:3000/signin&response_type=code&scope=read:user+user:email`;
+export async function GET(request: NextRequest) {
+  const redirectUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_AUTH_CLIENT_ID}&redirect_uri=${BASE_URL}/signin&response_type=code&scope=read:user+user:email`;
   return NextResponse.redirect(redirectUrl);
 }
